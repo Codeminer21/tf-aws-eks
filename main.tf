8ab0f1f54502622eddef93b592ee31f4966b16a2
gsilva <gaa.silva53@gmail.com>
2023-05-11 14:41:03 -0300

    fix: remove public access in cluster endpoint

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "eks" {
 
   vpc_id                         = var.vpc_id
   subnet_ids                     = var.private_subnets
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access = false
 
   eks_managed_node_group_defaults = {
     ami_type = var.ami_type
